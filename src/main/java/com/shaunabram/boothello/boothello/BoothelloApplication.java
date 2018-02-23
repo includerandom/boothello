985c7e41e52fa528b5e1e8bf6289a94afa7ea31d
sabram <sabram@lendingclub.com>
2018-02-22 21:44:08 -0800

    Initial commit

---
+++ src/main/java/com/shaunabram/boothello/boothello/BoothelloApplication.java
@@ -0,0 +1,12 @@
+package com.shaunabram.boothello.boothello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoothelloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoothelloApplication.class, args);
+	}
+}
